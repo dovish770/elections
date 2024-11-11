e1732f3d15ca8b75874e6b7c0b90614e2798352d
dovi stein <dovish@gmail.com>
2024-11-11 16:03:25 +0200

    connected to mongoos sfater creating schemas and added 4 candidates

---
+++ elections-backend/src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDb from "./config/MongoDb";
+
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+connectDb();
+// Routes
+
+// Error handling middleware
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
